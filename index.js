cc062f0dd9600a8439c2f68dcec555f890810953
Sagar Wankhade <105979852+Sagar1079@users.noreply.github.com>
2023-02-10 20:41:21 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ const jsonServer = require("json-server"); // importing json-server library
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
+const middlewaresCors = jsonServer.defaults({ noCors: true })
 const port = process.env.PORT || 3001; // you can use any port number here; i chose to use 3001
 
 server.use(middlewares);
+server.use(middlewaresCors);
 server.use(router);
 
 server.listen(port, () => {
